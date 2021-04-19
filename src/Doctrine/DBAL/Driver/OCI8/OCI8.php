36e7d85118d8c4395aaa089c56294044c1c5624d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-04-19 15:33:09 +0200

    refactor: Get rid of global scope constants.

--- src/Doctrine/DBAL/Driver/OCI8/OCI8.php
+++ src/Doctrine/DBAL/Driver/OCI8/OCI8.php
@@ -13,9 +13,6 @@ declare(strict_types=1);
 
 namespace EcPhp\DoctrineOci8\Doctrine\DBAL\Driver\OCI8;
 
-const PARAM_PREFIX = 0xA000;
-const PARAM_MAX = 0xAFFF;
-
 final class OCI8
 {
     public const PARAM_AFC = 0xA060;
@@ -53,11 +50,15 @@ final class OCI8
 
     public const PARAM_LVC = 0xA05E;
 
+    public const PARAM_MAX = 0xAFFF;
+
     public const PARAM_NTY = 0xA06C;
 
     public const PARAM_NUM = 0xA002;
 
     public const PARAM_ODT = 0xA09C;
+
+    public const PARAM_PREFIX = 0xA000;
 
     public const PARAM_ROWID = 0xA068;
 
@@ -73,11 +74,11 @@ final class OCI8
 
     public static function decodeParamConstant(int $value): int
     {
-        return self::isParamConstant($value) ? ($value & ~PARAM_PREFIX) : $value;
+        return self::isParamConstant($value) ? ($value & ~self::PARAM_PREFIX) : $value;
     }
 
     public static function isParamConstant(int $value): bool
     {
-        return PARAM_PREFIX <= $value && PARAM_MAX >= $value;
+        return self::PARAM_PREFIX <= $value && self::PARAM_MAX >= $value;
     }
 }
